8cd3e09cbba524e5c9566535e9707036a86d9b93
Rohanmore123 <97122575+Rohanmore123@users.noreply.github.com>
2024-08-08 06:55:29 +0000

    added html

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 app=Flask(__name__)
 
+
+projetcs=[
+  {
+    'id':1,
+    'title':'Time series Forecasting',
+    'description':'This is the first project'
+  },
+  {
+    'id':2,
+    'title':'Titanic survival prediction',
+    'description':'This is the second project'
+  },
+  {
+    'id':3,
+    'title':'Rag chatbot',
+    'description':'This is the third project'
+  }
+]
 @app.route('/')
 def hello_world():
-  return 'Hello World'
+  return render_template('home.html',project=projetcs)
 
 
 
